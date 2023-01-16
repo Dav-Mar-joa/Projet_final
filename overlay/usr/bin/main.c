121daf0fa453db12a75dd4ba29547facd45acea7
DM <DM@toto.com>
2023-01-16 15:52:06 +0100

    initialisation

---
+++ overlay/usr/bin/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("Entrez un caractère : \n");
+    scanf ("%c",&a);
+    printf("Caractère en code ASCII : %d \n", a);
+
+    return 0;
+}
